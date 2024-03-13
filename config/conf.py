5b1ecf31af3f5e99038dfbdb32f72879db99a5f5
Alex <koehlale@b-tu.de>
2024-03-13 11:17:58 +0100

    adding validators p.1

--- config/conf.py
+++ config/conf.py
@@ -1,20 +1,20 @@
 import logging
 
-from pydantic import Field
+from pydantic import Field, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="settings.env", extra="allow")
+    model_config = SettingsConfigDict(env_file="settings.env")
 
-    feature_generator: str = Field(
-        alias="FEATURE_GENERATOR",
-    )
+    feature_generator: str = Field(alias="FEATURE_GENERATOR")
     normalizer: str = Field(alias="NORMALIZER")
     eigenvec_func: str = Field(alias="EIGENVEC_FUNCTION")
-    sort_ev: bool = Field(alias="SORTING_EIGENVALUES")
-    number_of_bins: int = Field(alias="NUMBER_OF_BINS")
-    ratio_of_train_data: int = Field(alias="RATIO_OF_TRAIN_DATA")
+    sort_ev: bool = Field(
+        alias="SORTING_EIGENVALUES",
+    )
+    number_of_bins: int = Field(alias="NUMBER_OF_BINS", ge=1)
+    ratio_of_train_data: int = Field(alias="RATIO_OF_TRAIN_DATA", ge=0, le=100)
     data_output_folder: str = Field(alias="DATA_OUTPUT_FOLDER")
 
     @property
@@ -27,3 +27,13 @@ class Settings(BaseSettings):
     def name_str(self) -> str:
         sorted = "sort" if self.sort_ev else "unsort"
         return f"{self.normalizer}_{self.eigenvec_func}_{sorted}"
+
+    @field_validator("normalizer")
+    @classmethod
+    def check_correct_normalizer(cls, v: str) -> str:
+        possible_input = ["max", "mean"]
+        if v not in possible_input:
+            raise ValueError(
+                f"{v} is not a valid option. Chose one of {possible_input}."
+            )
+        return v
